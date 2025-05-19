0f0ed963bdd86260c3121f4b433d444638275f52
Ajay <ajaykumawat9904@gmail.com>
2025-05-19 12:38:32 +0530

    chore(client): remove unnecessary .gitkeep file from client directory

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -4,13 +4,7 @@ import { User } from "../models/User";
 export default class UserController {
     public static async getAll(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            const users = await User.find({}, {}, { path: 'hostCompany', select: ['name'] });
-
-            if (users.length === 0) {
-                res.status(404).json({ message: "No users found" });
-            }
-
-            res.status(200).json(users);
+            console.log("Testing getAll method");
             
         } catch (error) {
             console.error(`UserController.getAll() -> Error: ${error}`);
