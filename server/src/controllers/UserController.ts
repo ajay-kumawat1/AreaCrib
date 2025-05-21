436f2ee4720d1acd484718a497e1af4e5def5cf9
Ajay <ajaykumawat9904@gmail.com>
2025-05-21 09:01:35 +0530

    ref(UserController): remove unused User model import

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import { User } from "../models/User";
 
 export default class UserController {
     public static async getAll(req: Request, res: Response, next: NextFunction): Promise<void> {
