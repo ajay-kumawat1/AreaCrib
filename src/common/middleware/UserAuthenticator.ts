6261127adc5cbb398e7cfa006e9a9822ec54163e
Ajay <ajaykumawat9904@gmail.com>
2025-07-07 10:13:17 +0530

    feat(auth): add lodash dependency and enhance JWT validation middleware

--- src/common/middleware/UserAuthenticator.ts
+++ src/common/middleware/UserAuthenticator.ts
@@ -2,28 +2,72 @@ import { NextFunction, Request, Response } from "express";
 import { sendResponse } from "../../utils/common";
 import { RESPONSE_CODE, RESPONSE_FAILURE } from "../interfaces/Constants";
 import { validateAuth0JWT } from "./ValidAuth0Jwt";
+import { UserRole } from "../enum/Role";
 
 export default class UserAuthenticator {
-    /**
-     * Middleware function to check if the user is authenticated.
-     */
-    public static isAuthenticated(): Array<Function> {
-        return [
-            UserAuthenticator.validateJWT,
-            ...validateAuth0JWT()
-        ];
-    }
+  /**
+   * Middleware function to check if the user is authenticated.
+   */
+  public static isAuthenticated(): Array<Function> {
+    return [UserAuthenticator.validateJWT, ...validateAuth0JWT()];
+  }
 
-    protected static async validateJWT(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-        try {
-            const token = req.header('x-auth-token') ?? null;
-            if (token) {
-                next();
-            } else {
-                return sendResponse(res, {}, 'Authentication token is missing', RESPONSE_FAILURE, RESPONSE_CODE.UNAUTHORISED);
-            }
-        } catch (error) {
-            return sendResponse(res, {}, 'Internal server error', RESPONSE_FAILURE, RESPONSE_CODE.INTERNAL_SERVER_ERROR);
-        }
+  protected static async validateJWT(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    try {
+      const token = req.header("x-auth-token") ?? null;
+      if (token) {
+        next();
+      } else {
+        return sendResponse(
+          res,
+          {},
+          "Authentication token is missing",
+          RESPONSE_FAILURE,
+          RESPONSE_CODE.UNAUTHORISED
+        );
+      }
+    } catch (error) {
+      return sendResponse(
+        res,
+        {},
+        "Internal server error",
+        RESPONSE_FAILURE,
+        RESPONSE_CODE.INTERNAL_SERVER_ERROR
+      );
     }
+  }
+
+  protected static async isAdminLoggedIn(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const userDetails = req.user;
+      if (typeof userDetails === "object" && userDetails !== null && (userDetails as any)?.role === UserRole.ADMIN) {
+        req.user = userDetails;
+        next();
+      } else {
+        return sendResponse(
+          res,
+          {},
+          "You are not authorized to perform this action",
+          RESPONSE_FAILURE,
+          RESPONSE_CODE.UNAUTHORISED
+        );
+      }
+    } catch (error) {
+      return sendResponse(
+        res,
+        {},
+        "Internal server error",
+        RESPONSE_FAILURE,
+        RESPONSE_CODE.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
 }
