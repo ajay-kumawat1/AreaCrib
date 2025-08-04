208bdccf23edbc4ca473e4044c379907ab39319a
Ajay <ajaykumawat9904@gmail.com>
2025-08-04 16:10:07 +0530

    feat(property): update Property model to use LocalityId and add additional fields

--- src/models/Property.ts
+++ src/models/Property.ts
@@ -2,17 +2,21 @@ import { model, Schema, Document } from "mongoose";
 
 interface IProperty {
   title: string;
-  areaId: Schema.Types.ObjectId;
+  LocalityId: Schema.Types.ObjectId;
   price: number;
   size: number;
   bedrooms: number;
   bathrooms: number;
+  halls: number;
+  floors: number;
+  furnished: boolean;
+  createdBy: Schema.Types.ObjectId;
   amenities: string[];
   images: string[];
   description: string;
 }
 
-export type IPropertyDoc = Document<unknown, {}, IProperty> & IProperty;
+export interface IPropertyDoc extends IProperty, Document {}
 
 // CRUD TYPES
 export type UpdatePropertyBody = Partial<IProperty>;
@@ -21,11 +25,19 @@ export type NewPropertyDoc = Omit<IProperty, "created">;
 const propertySchema = new Schema<IPropertyDoc>(
   {
     title: { type: String, required: true },
-    areaId: { type: Schema.Types.ObjectId, ref: "Area", required: true },
+    LocalityId: {
+      type: Schema.Types.ObjectId,
+      ref: "Locality",
+      required: true,
+    },
     price: { type: Number, required: true },
     size: { type: Number, required: true },
     bedrooms: { type: Number, required: true },
     bathrooms: { type: Number, required: true },
+    halls: { type: Number, required: true },
+    floors: { type: Number, required: true },
+    furnished: { type: Boolean, default: false },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
     amenities: { type: [String], required: true },
     images: { type: [String], required: true },
     description: { type: String, required: true },
